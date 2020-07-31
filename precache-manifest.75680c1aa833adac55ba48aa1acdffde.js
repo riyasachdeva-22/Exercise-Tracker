0268048ec236bf65f78348967ade548d49cb0e75
riyasachdeva-22 <riya171674.cse@chitkara.edu.in>
2020-07-31 23:54:35 +0530

    Updates

---
+++ precache-manifest.75680c1aa833adac55ba48aa1acdffde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a00fe597245eb7b600fbd9575327c16",
+    "url": "/Exercise-Tracker/index.html"
+  },
+  {
+    "revision": "1e8b3e6edac7a4a155f0",
+    "url": "/Exercise-Tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1e8b3e6edac7a4a155f0",
+    "url": "/Exercise-Tracker/static/js/2.7d5c3e0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Exercise-Tracker/static/js/2.7d5c3e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f15134bd213a6dfeed5",
+    "url": "/Exercise-Tracker/static/js/main.b2b376cf.chunk.js"
+  },
+  {
+    "revision": "1bfcd42be915246f447a",
+    "url": "/Exercise-Tracker/static/js/runtime-main.d378c0bd.js"
+  }
+]);
